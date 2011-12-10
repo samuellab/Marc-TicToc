b4b175477fb044b88796754df09d2335da83f3ad
Marc Gershow <mgershow@fas.harvard.edu>
2011-12-10 16:30:57 -0500

    added copyright information

--- tictoc.cpp
+++ tictoc.cpp
@@ -3,6 +3,9 @@
  * Author: Marc
  * 
  * Created on December 3, 2009, 12:07 PM
+ * (C) Marc Gershow; licensed under the Creative Commons Attribution Share Alike 3.0 United States License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/us/ or send a letter to
+ * Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
  */
 
 #include "tictoc.h"
